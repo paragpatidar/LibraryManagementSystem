50b452f543af425a8d4fee491f9cdd2e14831897
Patidar <parag.patidar@capgemini.com>
2017-11-07 15:32:50 +0530

    added

---
+++ LibraryManagementSystemSpring/src/com/cg/library/exception/LibraryException.java
@@ -0,0 +1,35 @@
+package com.cg.library.exception;
+
+public class LibraryException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4089106971179484543L;
+
+	public LibraryException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LibraryException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LibraryException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LibraryException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LibraryException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
